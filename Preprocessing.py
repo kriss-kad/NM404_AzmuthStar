754b7f12aa450bee60e41003bc80a033bb6f8cad
prash312 <66882206+prash312@users.noreply.github.com>
2020-08-01 14:49:17 +0530

    Update Preprocessing.py
    added gdal_translate utility to make image  visible

--- Preprocessing.py
+++ Preprocessing.py
@@ -9,3 +9,16 @@ dst_raw = in_file.split("/")
 dst_raw = dst_raw[-1]
 warpConvert = "gdalwarp -of GTiff -t_srs EPSG:3857 " + str(in_file) + " " + "Processes\\raw\\"+ str(dst_raw)
 os.system(warpConvert)
+
+def Gdal_visible(dst_raw):    
+
+    out_file = "Processes\\raw\\"+str(dst_raw)
+
+    gdal_translate = "gdal_translate -of GTiff -ot Byte -scale 0 700 0 255 " + out_file + " " + "Processes\\Visible\\" + str(dst_raw)
+    os.system(gdal_translate)
+#########################
+
+
+#call function and pass file name
+
+#Gdal_visible('Filename')

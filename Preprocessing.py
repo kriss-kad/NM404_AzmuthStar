621c59a97c4863ce8e43f356cafbf6ca12746a6a
Rushikesh Aware <55570903+rushikeshaware@users.noreply.github.com>
2020-08-02 13:53:59 +0530

    Update Preprocessing.py
    Final Code

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,28 +1,25 @@
 import gdal
-import os 
+import os
 
-infile = input()
-#For Windows 
-dst_raw = in_file.split("\\")
-#For Linux
-dst_raw = in_file.split("/")
-dst_raw = dst_raw[-1]
-warpConvert = "gdalwarp -of GTiff -t_srs EPSG:3857 " + str(in_file) + " " + "Processes\\raw\\"+ str(dst_raw)
-os.system(warpConvert)
+#Convert original file to EPSG:3857
+def GdalWarp_Convert(in_file):    
+    in_file = in_file
+    dst_raw = in_file.split("\\")
+    dst_raw = dst_raw[-1]
 
+    warpConvert = "gdalwarp -of GTiff -t_srs EPSG:3857 " + str(in_file) + " " + "Processes\\raw\\"+ str(dst_raw)
+    os.system(warpConvert)
+    return str(dst_raw)
+
+#Convert EPSG output to visble
 def Gdal_visible(dst_raw):    
 
     out_file = "Processes\\raw\\"+str(dst_raw)
 
     gdal_translate = "gdal_translate -of GTiff -ot Byte -scale 0 700 0 255 " + out_file + " " + "Processes\\Visible\\" + str(dst_raw)
     os.system(gdal_translate)
-#########################
 
-
-#call function and pass file name
-
-#Gdal_visible('Filename')
-
+#
 def Water_Extraction(src_file):
     out_dst = src_file.split("\\")
     out_dst = str(out_dst[-1])
@@ -32,7 +29,7 @@ def Water_Extraction(src_file):
 
     extraction = "gdalwarp -of GTiff -t_srs EPSG:3857 -cutline " + shapefile + " " + "-cwhere "+"\"ID='39'\"" + " " + src_file +" "+ "Processes\\Ocean\\"+out_dst
     os.system(extraction)
-    
+
 def Land_Extraction(src_file):
     out_dst = src_file.split("\\")
     out_dst = str(out_dst[-1])
@@ -42,7 +39,7 @@ def Land_Extraction(src_file):
 
     extraction = "gdalwarp -of GTiff -t_srs EPSG:3857 -cutline " + shapefile + " " + "-cwhere "+"\"ID='39'\"" + " " + src_file +" "+ "Processes\\Land\\"+out_dst
     os.system(extraction)
-    
+
 #call this fuction, it will convert given in_file to EPSG:3857 and store its output to process/raw/ dir,
 # Then it will pick converted file and perform gdal_translate and store its output to process/visible/ dir.
 def convert_and_visible(in_file):
@@ -53,8 +50,7 @@ def convert_and_visible(in_file):
     print(dst_raw)
     Gdal_visible(dst_raw)
     print("Pixel Scaling Done")
-    
+
 #import sys
 #filename = str(sys.argv[1])
 #convert_and_visible(filename)
-

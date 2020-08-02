228ea15e910394e56b880c5ccff4435e8772d97b
kriss-kad <69065285+kriss-kad@users.noreply.github.com>
2020-08-02 17:49:36 +0530

    Update Preprocessing.py
    Done with the Preprocessing

--- Preprocessing.py
+++ Preprocessing.py
@@ -20,15 +20,40 @@ def Gdal_visible(dst_raw):
     os.system(gdal_translate)
 
 #
+##def Water_Extraction(src_file):
+##    out_dst = src_file.split("\\")
+##    out_dst = str(out_dst[-1])
+##
+##    #mention shapefile path here
+##    shapefile = "Processes\\Shapefile\\Water\\World_Seas_IHO_v3.shp"
+##    faster = r"-co TILED=YES -co 'BLOCKXSIZE=4096' -co 'BLOCKYSIZE=4096' -multi -co NUM_THREADS=ALL_CPUS -wo NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 9000 -wm 9000"
+##    extraction = "gdalwarp -of GTiff -cutline " + shapefile + " " + "-cwhere "+"\"ID='39'\"" + " " + faster + src_file +" "+ "Processes\\Ocean\\"+out_dst
+##    os.system(extraction)
+
 def Water_Extraction(src_file):
-    out_dst = src_file.split("\\")
-    out_dst = str(out_dst[-1])
+        cwd = os.getcwd() 
+        gdalwarp = r'gdalwarp'
+        cutline = " -cutline" 
+        shapefile = r"Processes\\Shapefile\\Water\\World_Seas_IHO_v3.shp"
+        sql = ' -cwhere \"ID=\'39\'\"'
+        tiled = r" -co TILED=YES"
+        blocksizeX = r" -co BLOCKXSIZE=4096"
+        blocksizeY = r" -co BLOCKYSIZE=4096"
+        CPU_Threads = r" -co NUM_THREADS=ALL_CPUS"
+        num_threads = r" -wo NUM_THREADS=ALL_CPUS"
+        max_cache = r" --config GDAL_CACHEMAX " + "9000" + " " +  "-wm" +" "+ "9000"
+        infile = src_file
+        #print(infile)
+        dst = r"processes\\Ocean\\" + src_file.split("\\")[-1]
+        #print(src_file)
+        #check if file exist
+        if os.path.isfile(dst):
+            print(dst,"File Already Exist")
+        else:
+            fullCmd = gdalwarp  + cutline + " "+ shapefile + sql + tiled + blocksizeX + blocksizeY + CPU_Threads + num_threads + max_cache + " "+ infile + " " + cwd + "\\" + dst
+            print(fullCmd)
+            os.system(fullCmd)
 
-    #mention shapefile path here
-    shapefile = "Processes\\Shapefile\\Water\\World_Seas_IHO_v3.shp"
-
-    extraction = "gdalwarp -of GTiff -t_srs EPSG:3857 -cutline " + shapefile + " " + "-cwhere "+"\"ID='39'\"" + " " + src_file +" "+ "Processes\\Ocean\\"+out_dst
-    os.system(extraction)
 
 def Land_Extraction(src_file):
     out_dst = src_file.split("\\")
